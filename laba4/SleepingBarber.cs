3f17fb74baa5dd903edcd28c9380f8c9d446d7e1
daniil <daniil@pisarev.server.test>
2025-12-24 19:14:22 +0300

    Make SleepingBarberDemo class in new file

--- laba4/SleepingBarber.cs
+++ laba4/SleepingBarber.cs
@@ -76,29 +76,3 @@ class BarberShop
     }
 }
 
-class SleepingBarberDemo
-{
-    public static void Run()
-    {
-        BarberShop shop = new BarberShop(3);
-        Thread barberThread = new Thread(() => shop.BarberWork());
-        barberThread.Start();
-        Thread.Sleep(500);
-        Random random = new Random();
-        for (int i = 1; i <= 10; i++)
-        {
-            int customerId = i;
-            Thread customerThread = new Thread(() =>
-            {
-                Thread.Sleep(random.Next(100, 400));
-                shop.CustomerArrives(customerId);
-            });
-            customerThread.Start();
-        }
-        
-        Thread.Sleep(15000);
-        shop.Close();
-        barberThread.Join();
-    }
-}
-
